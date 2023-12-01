26307118f502744510c781d87fe2f172e5db3d37
md-merajuddin-ceq <151141290+Merajuddin-ceq@users.noreply.github.com>
2023-12-01 22:51:45 +0530

    Add files via upload

---
+++ footer/footer.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import SmartphoneOutlinedIcon from '@mui/icons-material/SmartphoneOutlined';
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+
+}
